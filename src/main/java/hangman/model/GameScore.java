4456f9dadbc349afebe87627c368aa3bde9ac192
Daniel19902 <60512110+Daniel19902@users.noreply.github.com>
2020-08-29 21:44:05 -0500

    correccion lab

--- src/main/java/hangman/model/GameScore.java
+++
@@ -1,9 +0,0 @@
-package hangman.model;
-
-import hangman.GameScoreException;
-
-public interface GameScore {
-
-    public int calculateScore(int correctCount, int incorrectCount) throws GameScoreException;
-
-}
